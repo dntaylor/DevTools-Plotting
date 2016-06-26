22edec8a6a26299007fb9fd680c947fc4306fc60
Devin Taylor <dntaylor@wisc.edu>
2016-06-26 05:34:30 -0500

    latest ntuples

--- python/histParams.py
+++ python/histParams.py
@@ -16,6 +16,18 @@ version = getCMSSWVersion()
 ### hists ###
 #############
 histParams = {
+    # overrides for Electron
+    'Electron': {
+        'count'                       : {'xVariable': '1',                              'xBinning': [1,0,2],                 }, # just a count of events passing selection
+        'pt'                          : {'xVariable': 'e_pt',                           'xBinning': [2000,0,2000],           },
+        'eta'                         : {'xVariable': 'e_eta',                          'xBinning': [600,-3.,3.],            },
+    },
+    # overrides for Muon
+    'Muon': {
+        'count'                       : {'xVariable': '1',                              'xBinning': [1,0,2],                 }, # just a count of events passing selection
+        'pt'                          : {'xVariable': 'm_pt',                           'xBinning': [2000,0,2000],           },
+        'eta'                         : {'xVariable': 'm_eta',                          'xBinning': [600,-3.,3.],            },
+    },
     # overrides for Tau
     'Tau': {
         'count'                       : {'xVariable': '1',                              'xBinning': [1,0,2],                 }, # just a count of events passing selection
@@ -290,62 +302,62 @@ genCut = '{0}_genMatch==1 && {0}_genDeltaR<0.1'
 fakeCut = '({0}_genMatch==0 || ({0}_genMatch==1 && {0}_genIsFromHadron && {0}_genDeltaR<0.1))'
 fakeTauCut = '({0}_genMatch==0 || ({0}_genMatch==1 && {0}_genDeltaR>0.1))'
 
-##########################
-#### electron specific ###
-##########################
-#selectionParams['Electron'] = {
-#    'default/prompt' : {'args': [promptCut.format('e')],                                       'kwargs': {}},
-#    'default/fake'   : {'args': [fakeCut.format('e')],                                         'kwargs': {}},
-#    'barrel/prompt'  : {'args': [' && '.join([promptCut.format('e'),eBarrelCut.format('e')])], 'kwargs': {}},
-#    'barrel/fake'    : {'args': [' && '.join([fakeCut.format('e'),eBarrelCut.format('e')])],   'kwargs': {}},
-#    'endcap/prompt'  : {'args': [' && '.join([promptCut.format('e'),eEndcapCut.format('e')])], 'kwargs': {}},
-#    'edncap/fake'    : {'args': [' && '.join([fakeCut.format('e'),eEndcapCut.format('e')])],   'kwargs': {}},
-#}
-#
-#sels = selectionParams['Electron'].keys()
-#idCuts = {
-#    'cutBasedVeto'   : 'e_cutBasedVeto==1',
-#    'cutBasedLoose'  : 'e_cutBasedLoose==1',
-#    'cutBasedMedium' : 'e_cutBasedMedium==1',
-#    'cutBasedTight'  : 'e_cutBasedTight==1',
-#    'wwLoose'        : 'e_wwLoose==1',
-#    'heepV60'        : 'e_heepV60==1',
-#    'NonTrigWP80'    : 'e_mvaNonTrigWP80==1',
-#    'NonTrigWP90'    : 'e_mvaNonTrigWP90==1',
-#    'TrigPre'        : 'e_mvaTrigPre==1',
-#    'TrigWP80'       : 'e_mvaTrigPre==1 && e_mvaTrigWP80==1',
-#    'TrigWP90'       : 'e_mvaTrigPre==1 && e_mvaTrigWP90==1',
-#}
-#for sel in sels:
-#    for idName in idCuts:
-#        name = '{0}/{1}'.format(sel,idName)
-#        selectionParams['Electron'][name] = deepcopy(selectionParams['Electron'][sel])
-#        args = selectionParams['Electron'][name]['args']
-#        selectionParams['Electron'][name]['args'][0] = args[0] + ' && ' + idCuts[idName]
-#
-######################
-#### muon specific ###
-######################
-#selectionParams['Muon'] = {
-#    'default/prompt' : {'args': [promptCut.format('m')], 'kwargs': {}},
-#    'default/fake'   : {'args': [fakeCut.format('m')],   'kwargs': {}},
-#}
-#
-#sels = selectionParams['Muon'].keys()
-#idCuts = {
-#    'isLooseMuon_looseIso'  : 'm_isLooseMuon==1 && m_isolation<0.4',
-#    'isMediumMuon_tightIso' : 'm_isMediumMuon==1 && m_isolation<0.15',
-#    'isTightMuon_tightIso'  : 'm_isTightMuon==1 && m_isolation<0.15',
-#    'isHighPtMuon_tightIso' : 'm_isHighPtMuon==1 && m_isolation<0.15',
-#    'wzLooseMuon'           : 'm_isMediumMuon==1 && m_trackRelIso<0.4 && m_isolation<0.4',
-#    'wzMediumMuon'          : 'm_isMediumMuon==1 && m_trackRelIso<0.4 && m_isolation<0.15 && m_dz<0.1 && (m_pt<20 ? m_dxy<0.01 : m_dxy<0.02)',
-#}
-#for sel in sels:
-#    for idName in idCuts:
-#        name = '{0}/{1}'.format(sel,idName)
-#        selectionParams['Muon'][name] = deepcopy(selectionParams['Muon'][sel])
-#        args = selectionParams['Muon'][name]['args']
-#        selectionParams['Muon'][name]['args'][0] = args[0] + ' && ' + idCuts[idName]
+#########################
+### electron specific ###
+#########################
+selectionParams['Electron'] = {
+    'default/prompt' : {'args': [promptCut.format('e')],                                       'kwargs': {}},
+    'default/fake'   : {'args': [fakeCut.format('e')],                                         'kwargs': {}},
+    #'barrel/prompt'  : {'args': [' && '.join([promptCut.format('e'),eBarrelCut.format('e')])], 'kwargs': {}},
+    #'barrel/fake'    : {'args': [' && '.join([fakeCut.format('e'),eBarrelCut.format('e')])],   'kwargs': {}},
+    #'endcap/prompt'  : {'args': [' && '.join([promptCut.format('e'),eEndcapCut.format('e')])], 'kwargs': {}},
+    #'edncap/fake'    : {'args': [' && '.join([fakeCut.format('e'),eEndcapCut.format('e')])],   'kwargs': {}},
+}
+
+sels = selectionParams['Electron'].keys()
+idCuts = {
+    'cutBasedVeto'   : 'e_cutBasedVeto==1',
+    'cutBasedLoose'  : 'e_cutBasedLoose==1',
+    'cutBasedMedium' : 'e_cutBasedMedium==1',
+    'cutBasedTight'  : 'e_cutBasedTight==1',
+    'wwLoose'        : 'e_wwLoose==1',
+    'heepV60'        : 'e_heepV60==1',
+    'NonTrigWP80'    : 'e_mvaNonTrigWP80==1',
+    'NonTrigWP90'    : 'e_mvaNonTrigWP90==1',
+    'TrigPre'        : 'e_mvaTrigPre==1',
+    'TrigWP80'       : 'e_mvaTrigPre==1 && e_mvaTrigWP80==1',
+    'TrigWP90'       : 'e_mvaTrigPre==1 && e_mvaTrigWP90==1',
+}
+for sel in sels:
+    for idName in idCuts:
+        name = '{0}/{1}'.format(sel,idName)
+        selectionParams['Electron'][name] = deepcopy(selectionParams['Electron'][sel])
+        args = selectionParams['Electron'][name]['args']
+        selectionParams['Electron'][name]['args'][0] = args[0] + ' && ' + idCuts[idName]
+
+#####################
+### muon specific ###
+#####################
+selectionParams['Muon'] = {
+    'default/prompt' : {'args': [promptCut.format('m')], 'kwargs': {}},
+    'default/fake'   : {'args': [fakeCut.format('m')],   'kwargs': {}},
+}
+
+sels = selectionParams['Muon'].keys()
+idCuts = {
+    'isLooseMuon_looseIso'  : 'm_isLooseMuon==1 && m_isolation<0.4',
+    'isMediumMuon_tightIso' : 'm_isMediumMuon==1 && m_isolation<0.15',
+    'isTightMuon_tightIso'  : 'm_isTightMuon==1 && m_isolation<0.15',
+    'isHighPtMuon_tightIso' : 'm_isHighPtMuon==1 && m_isolation<0.15',
+    'wzLooseMuon'           : 'm_isMediumMuon==1 && m_trackRelIso<0.4 && m_isolation<0.4',
+    'wzMediumMuon'          : 'm_isMediumMuon==1 && m_trackRelIso<0.4 && m_isolation<0.15 && m_dz<0.1 && (m_pt<20 ? m_dxy<0.01 : m_dxy<0.02)',
+}
+for sel in sels:
+    for idName in idCuts:
+        name = '{0}/{1}'.format(sel,idName)
+        selectionParams['Muon'][name] = deepcopy(selectionParams['Muon'][sel])
+        args = selectionParams['Muon'][name]['args']
+        selectionParams['Muon'][name]['args'][0] = args[0] + ' && ' + idCuts[idName]
 
 ####################
 ### tau specific ###
