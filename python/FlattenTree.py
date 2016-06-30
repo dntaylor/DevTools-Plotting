f03f9e894dbec94a41d0344cd0955ed8b5ffb57b
Devin Taylor <dntaylor@wisc.edu>
2016-06-30 09:30:24 -0500

    more verbose

--- python/FlattenTree.py
+++ python/FlattenTree.py
@@ -72,5 +72,5 @@ class FlattenTree(object):
         else:
             n = len(allJobs)
             for i,args in enumerate(allJobs):
-                logging.info('Processing plot {0} of {1}: {2}.'.format(i,n,' '.join(args)))
+                logging.info('Processing {3} {4} plot {0} of {1}: {2}.'.format(i,n,' '.join(args),self.analysis,self.sample))
                 self.ntuple.flatten(*args)
