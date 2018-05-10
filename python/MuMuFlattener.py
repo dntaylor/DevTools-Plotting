abcc1d9cb4fa541d1eb49b78837b5d45ff40a9df
Devin Taylor <dntaylor@ucdavis.edu>
2018-05-10 17:34:52 -0500

    selections

--- python/MuMuFlattener.py
+++ python/MuMuFlattener.py
@@ -38,45 +38,58 @@ class MuMuFlattener(NtupleFlattener):
 
         # controls
         self.doThreeMuon = True
+        self.doDeltaR = True
+        self.doIso = True
+        self.doAntiIso = True
 
 
         # setup properties
         self.leps = ['m1','m2']
         self.baseCutMap = {
-            'mmWindow'  : lambda row: row.mm_mass>1 and row.amm_mass<30,
+            'mmWindow'  : lambda row: row.mm_mass>1 and row.mm_mass<30,
             'trigger'   : lambda row: row.m1_matches_IsoMu24 or row.m1_matches_IsoTkMu24,
-            #'mmDR'       : lambda row: row.mm_deltaR<1,
         }
+
+        self.cuts = {}
+        if self.doThreeMuon: self.cuts['threeMuon'] = lambda row: row.numMuons>2
+        if self.doDeltaR:    self.cuts['deltaR']    = lambda row: row.mm_deltaR<1
+        if self.doIso:       self.cuts['iso']       = lambda row: row.m2_isolation<0.25
+        if self.doAntiIso:   self.cuts['antiiso']   = lambda row: row.m2_isolation>0.25
 
         self.selectionMap = {}
         baseSels = []
         self.selectionMap['default'] = lambda row: all([self.baseCutMap[cut](row) for cut in self.baseCutMap])
         baseSels += ['default']
-        if self.doThreeMuon:
-            self.selectionMap['threeMuon/default']  = lambda row: row.numMuons>2  and all([self.baseCutMap[cut](row) for cut in self.baseCutMap])
-            baseSels += ['threeMuon']
 
         self.selections = []
         self.selectionHists = {}
         for sel in self.selectionMap:
             self.selections += [sel]
+            
+        for i in range(len(self.cuts)):
+            for cuts in itertools.combinations(self.cuts,i+1):
+                if 'iso' in cuts and 'antiiso' in cuts: continue
+                name = '_'.join(sorted(cuts))
+                cutsel = '{}/default'.format(name)
+                self.selections += [cutsel]
+
 
         # setup histogram parameters
         self.histParams = {
-            'count'                       : {'x': lambda row: 1,                                  'xBinning': [1,0,2],                 }, # just a count of events passing selection
-            'numVertices'                 : {'x': lambda row: row.numVertices,                    'xBinning': [60,0,60],               },
-            'met'                         : {'x': lambda row: row.met_pt,                         'xBinning': [500, 0, 500],           },
-            'metPhi'                      : {'x': lambda row: row.met_phi,                        'xBinning': [50, -3.14159, 3.14159], },
-            # amm
-            'mmMass'                     : {'x': lambda row: row.amm_mass,                       'xBinning': [3000, 0, 30],           },
-            'mmDeltaR'                   : {'x': lambda row: row.amm_deltaR,                     'xBinning': [100, 0, 1.5],           },
-            'mmDeltaPhi'                 : {'x': lambda row: abs(row.amm_deltaPhi),              'xBinning': [500, 0, 3.14159],       },
-            'm1Pt'                       : {'x': lambda row: row.am1_pt,                         'xBinning': [500, 0, 500],           },
-            'm2Pt'                       : {'x': lambda row: row.am2_pt,                         'xBinning': [500, 0, 500],           },
-            'm1Eta'                      : {'x': lambda row: row.am1_eta,                        'xBinning': [100, -2.5, 2.5],        },
-            'm2Eta'                      : {'x': lambda row: row.am2_eta,                        'xBinning': [100, -2.5, 2.5],        },
-            'm1Iso'                      : {'x': lambda row: row.am1_isolation,                  'xBinning': [100, 0, 2],             },
-            'm2Iso'                      : {'x': lambda row: row.am2_isolation,                  'xBinning': [100, 0, 2],             },
+            #'count'                       : {'x': lambda row: 1,                                  'xBinning': [1,0,2],                 }, # just a count of events passing selection
+            #'numVertices'                 : {'x': lambda row: row.numVertices,                    'xBinning': [60,0,60],               },
+            #'met'                         : {'x': lambda row: row.met_pt,                         'xBinning': [500, 0, 500],           },
+            #'metPhi'                      : {'x': lambda row: row.met_phi,                        'xBinning': [50, -3.14159, 3.14159], },
+            # mm
+            'mmMass'                     : {'x': lambda row: row.mm_mass,                        'xBinning': [3000, 0, 30],           },
+            'mmDeltaR'                   : {'x': lambda row: row.mm_deltaR,                      'xBinning': [100, 0, 1.5],           },
+            #'mmDeltaPhi'                 : {'x': lambda row: abs(row.mm_deltaPhi),               'xBinning': [500, 0, 3.14159],       },
+            'm1Pt'                       : {'x': lambda row: row.m1_pt,                          'xBinning': [500, 0, 500],           },
+            'm2Pt'                       : {'x': lambda row: row.m2_pt,                          'xBinning': [500, 0, 500],           },
+            'm1Eta'                      : {'x': lambda row: row.m1_eta,                         'xBinning': [100, -2.5, 2.5],        },
+            'm2Eta'                      : {'x': lambda row: row.m2_eta,                         'xBinning': [100, -2.5, 2.5],        },
+            'm1Iso'                      : {'x': lambda row: row.m1_isolation,                   'xBinning': [100, 0, 2],             },
+            'm2Iso'                      : {'x': lambda row: row.m2_isolation,                   'xBinning': [100, 0, 2],             },
         }
 
         # initialize flattener
@@ -86,11 +99,23 @@ class MuMuFlattener(NtupleFlattener):
 
     def perRowAction(self,row):
         isData = row.isData
-
+        w = 1
+        cutVals = {}
         for sel in self.selectionMap:
             result = self.selectionMap[sel](row)
             if result:
                 self.fill(row,sel,w)
+                for cut in self.cuts:
+                    cutVals[cut] = self.cuts[cut](row)
+                for i in range(len(self.cuts)):
+                    for cuts in itertools.combinations(self.cuts,i+1):
+                        if 'iso' in cuts and 'antiiso' in cuts: continue
+                        name = '_'.join(sorted(cuts))
+                        cutsel = '{}/default'.format(name)
+                        cutresult = all([cutVals[c] for c in cuts])
+                        if cutresult:
+                            self.fill(row,cutsel,w)
+                
 
 
 def parse_command_line(argv):
